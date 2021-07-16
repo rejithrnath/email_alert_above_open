7f9f59b0c6d26a99a43c1de548fe08b267fcb312
Rejith Reghunathan <220777@usn.no>
2021-07-16 09:50:34 +0200

    Update code to use scheduler instead of while loop

--- email_above_open.py
+++ email_above_open.py
@@ -9,6 +9,7 @@ import datetime
 import yfinance as yf
 import time
 import smtplib, ssl
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 #add ticker and average price on below dictionary 
 stocks ={
@@ -34,23 +35,23 @@ password = "enter_password"
 
 
 # time duration for trading
-trading_start_time= "09:00"
-trading_end_time = "22:00"
+trading_start_time_hour= 10
+trading_end_time_hour = 22
 
 
-def is_between(time, time_range):
-    if time_range[1] < time_range[0]:
-        return time >= time_range[0] or time <= time_range[1]
-    return time_range[0] <= time <= time_range[1]
 
 
-while True:
+sched = BlockingScheduler()
+@sched.scheduled_job('interval',hours=1)
+def download_and_email():
     ohlcv_data ={}
     print(datetime.datetime.now())
     for ticker in stocks.keys():
         ohlcv_data[ticker] = yf.download(ticker,start,end, interval=interval_duration, progress = False)
         trading_current_time = str(datetime.datetime.now().hour)+":"+str(datetime.datetime.now().minute)
-        if ohlcv_data[ticker]["Adj Close"][-1] > stocks[ticker] and is_between(trading_current_time, (trading_start_time, trading_end_time)) ==True:
+        if (ohlcv_data[ticker]["Adj Close"][-1] > stocks[ticker])\
+            and ((datetime.datetime.now().hour >= trading_start_time_hour) and\
+            (datetime.datetime.now().hour <= trading_end_time_hour))== True:
            print(f'{ticker} is above. Avg.Value = {stocks[ticker]} and Current value = {ohlcv_data[ticker]["Adj Close"][-1]} ')
            message = f'{ticker} is above. Avg.Value = {stocks[ticker]} and Current value = Current value {ohlcv_data[ticker]["Adj Close"][-1]} '
            context = ssl.create_default_context()
@@ -60,6 +61,10 @@ while True:
             server.ehlo()  # Can be omitted
             server.login(sender_email, password)
             server.sendmail(sender_email, receiver_email, message)
- 
+            print("Email sent.")
     print("\n")
-    time.sleep(time_sleep_input)
+sched.start()
+    
+ 
+   
+    
